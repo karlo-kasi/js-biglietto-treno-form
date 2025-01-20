b0213fe568235658a13cd63022dec8a0d46ba6d8
karlo-kasi <karlokasi1995@gmail.com>
2025-01-20 16:34:35 +0100

    Add: js function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 let nameField = document.getElementById ( "name-field" );
 let kmField = document.getElementById ( "km-field" );
 let etaField = document.getElementById ( "select-field" );
-let form = document.getElementById ( "form" );
+let btn = document.getElementById ( "btn" );
 // let btnField = document.getElementById ( "btn" )
 
 //Vado a prendermi i valori del biglietto dove stamparli
@@ -13,29 +13,47 @@ let codice = document.getElementById ( "codice" )
 let costoBiglietto = document.getElementById ( "costo-biglietto" )
 
 // variabili già presenti
-let numeroRandomCarrozza = (Match.floor(Match.random() * 10))
+let numeroRandomCarrozza = (Math.floor(Math.random() * 10))
 let numeroRandomCodice = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
 const euro = 0.21;
-let prezzoBiglietto = (km * euro);
-let prezzoBigliettoSconto1 = (prezzoBiglietto * 0.8)
-let prezzoBigliettoSconto2 = (prezzoBiglietto * 0.6)
+
+
 
 //creo la funzione
-form.addEventListeren ( 'click', function(){
+btn.addEventListener ( 'click', function(event){
     
-    nome.innerHTML = nameField.value
-    select.innerHTML = etaField.value
-    carrozza.innerHTML = 
+    event.preventDefault()
+
+    let nomePassegero = nameField.value
+    let eta = etaField.value
+    let km = parseFloat(kmField.value)
 
 
+    let prezzoBiglietto = km * euro;
+    let prezzoBigliettoSconto1 = prezzoBiglietto * 0.8
+    let prezzoBigliettoSconto2 = prezzoBiglietto * 0.6
+    let prezzoFinale 
+    
 
-    if (etaField < 18) {
-        console.log(`${prezzoBigliettoSconto1.toFixed(2)} € è il prezzo del tuo biglietto`)
-    } else if (età >= 65) {
-        console.log(`${prezzoBigliettoSconto2.toFixed(2)} € è il prezzo del tuo biglietto`)
+
+    //condizione
+    if (eta == "min") {
+         prezzoFinale = prezzoBigliettoSconto1
+    } else if (eta == "over") {
+         prezzoFinale = prezzoBigliettoSconto2
     } else {
-        console.log(`${prezzoBiglietto.toFixed(2)} € è il prezzo del tuo biglietto`)
+         prezzoFinale = prezzoBiglietto
     }
+
+    console.log (prezzoFinale)
+
+    // inserisco i dati che mi ha dato il form
+    nome.innerHTML = nomePassegero
+    select.innerHTML = etaField.value
+    carrozza.innerHTML = numeroRandomCarrozza
+    codice.innerHTML = numeroRandomCodice
+    costoBiglietto.innerHTML = `${prezzoFinale} €`
+
 })
 
 
